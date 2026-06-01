0881b83c2d826ee48ec59a469a0d4b6635bfccab
SofiaL <167090740+SofiaVLopes@users.noreply.github.com>
2026-06-01 12:33:39 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,7 @@
+#caixa-resultado {
+    background-color: var(--fundo-card);
+    border-left: 5px solid var(--cor-terra);
+    padding: 2rem;
+    border-radius: 0 12px 12px 0;
+    max-width: 600px;
+}
